d4a9a5a02535fb1a2769f5a7e86e1060628fdce5
Domingo <domingohui@gmail.com>
2017-03-16 00:42:29 -0400

    Trim whitespace in input; use entire commit object
    Work on CSS
    Clear button

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 Vue.component('firstcommit', {
-    props: ['date'],
-    template: '<div>First commit: {{ date }}</div>'
+    props: ['commit'],
+    template: '<div>First commit: {{ commit }}</div>'
 });
 
 var prompt = new Vue({
     el: '#app',
     data: {
+        /* Model fields linked to input */
         username: '',
         repo: '',
+        /********************************/
         show_commit: false,
-        date: null
+        commit: null
     },
     methods: {
         fetch_first_commit: fetch_first_commit
@@ -17,16 +19,19 @@ var prompt = new Vue({
 });
 
 
-function fetch_first_commit (username, repo) {
-    let repo_base_url = 'https://api.github.com/repos/';
-    let call_url = repo_base_url + username + '/' + repo + '/commits';
-    fetch(call_url)
-        .then((response)=>response.json())
-        .then((data)=>{
-            let commit = get_earliest_commit(data);
-            this.date = commit.commit.committer.date;
-            this.show_commit = true;
-        });
+function fetch_first_commit () {
+    let username = this.username.trim();
+    let repo = this.repo.trim();
+    if (username && repo) {
+        let repo_base_url = 'https://api.github.com/repos/';
+        let call_url = repo_base_url + username + '/' + repo + '/commits';
+        fetch(call_url)
+            .then((response)=>response.json())
+            .then((data)=>{
+                this.commit = Object.assign({}, get_earliest_commit(data));
+                this.show_commit = true;
+            });
+    }
 }
 
 function get_earliest_commit(commits) {
