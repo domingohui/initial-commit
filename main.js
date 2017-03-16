0d4e633ac1f1fda3d57465102247dc64eb3b7c28
Domingo <domingohui@gmail.com>
2017-03-15 23:38:02 -0400

    First commit; trying out vue.js

---
+++ main.js
@@ -0,0 +1,34 @@
+Vue.component('firstcommit', {
+    props: ['date'],
+    template: '<div>First commit: {{ date }}</div>'
+});
+
+var prompt = new Vue({
+    el: '#app',
+    data: {
+        message: 'Enter username',
+        show_commit: false,
+        date: null
+    },
+    methods: {
+        fetch_first_commit: fetch_first_commit
+    },
+});
+
+
+function fetch_first_commit (username, repo) {
+    let repo_base_url = 'https://api.github.com/repos/';
+    let call_url = repo_base_url + username + '/' + repo + '/commits';
+    fetch(call_url)
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log(data);
+            let commit = get_earliest_commit(data);
+            this.date = commit.commit.committer.date;
+            this.show_commit = true;
+        });
+}
+
+function get_earliest_commit(commits) {
+    return commits[commits.length-1];
+}
